45595996963f430473e5295899539a936c12a73f
JoJorge <jorge13jorge13@gmail.com>
2018-04-14 21:59:31 +0800

    add Action Node and script for pull items from people

---
+++ Assets/Scripts/FSM/ThrowItems.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourMachine;
+
+[NodeInfo(category = "Action/Thief/")]
+public class ThrowItems : ActionNode {
+
+    public GameObjectVar target;
+    public Vector3Var throwPosition;
+
+	public override void Start () {
+        base.Start (); 
+        target.Value.GetComponent<Target>().emit(throwPosition.Value);
+    }
+
+    public override Status Update () {
+        return Status.Success;
+    }
+}
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Target : MonoBehaviour {
 
+    [SerializeField] private float minThrowScale;
+    [SerializeField] private float maxThrowScale;
+    [SerializeField] private float velocity;
     [SerializeField] private GameObject[] items;
     private List<Transform> dropItems;
 
 	// Use this for initialization
 	void Start () { 
+        dropItems = new List<Transform> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (dropItems.Count > 0) {
+            GetComponent<Rigidbody> ().velocity = (dropItems [0].transform.position - transform.position) * velocity;
+        }
+        else {
+            GetComponent<Rigidbody> ().velocity = Vector3.zero;
+        }
 	}
+
+    public void emit(Vector3 playerPosition) {
+        Vector3 dir = transform.position - playerPosition;
+        foreach (GameObject item in items) {
+            GameObject spawn = GameObject.Instantiate (item);
+            spawn.GetComponent<Rigidbody> ().AddForce (dir * Random.Range(minThrowScale, maxThrowScale));
+            dropItems.Add (spawn.transform);
+        }
+    }
 }
