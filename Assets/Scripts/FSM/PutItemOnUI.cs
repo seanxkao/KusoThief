81fc966b562ebb875763f017e863ee3df7693511
JoJorge <jorge13jorge13@gmail.com>
2018-04-15 12:04:53 +0800

    put ThrowItems to FSM, add PickItem and DropItem action

---
+++ Assets/Scripts/FSM/PutItemOnUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using BehaviourMachine;
+
+[NodeInfo(category = "Action/Thief/")]
+public class PutItemOnUI : ActionNode {
+    public GameObjectVar item;
+    public GameObjectVar UI;
+
+    public override void Start () {
+        base.Start ();
+        UI.Value.GetComponent<Image> ().sprite = item.Value.GetComponent<Item> ().getImage ();
+    }
+    public override Status Update () {
+        return Status.Success;
+    }
+}
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Target : MonoBehaviour {
 
     [SerializeField] private float minThrowScale;
@@ -25,12 +26,26 @@ public class Target : MonoBehaviour {
         }
 	}
 
-    public void emit(Vector3 playerPosition) {
-        Vector3 dir = transform.position - playerPosition;
+    public void emit(Transform player) {
+        Vector3 dir = player.position - transform.position;
         foreach (GameObject item in items) {
             GameObject spawn = GameObject.Instantiate (item);
-            spawn.GetComponent<Rigidbody> ().AddForce (dir * Random.Range(minThrowScale, maxThrowScale));
+            spawn.transform.position = transform.position + 3 * Vector3.up;
+            Rigidbody rigid = spawn.GetComponent<Rigidbody> ();
+            rigid.velocity = dir * Random.Range (minThrowScale, maxThrowScale);
             dropItems.Add (spawn.transform);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (dropItems.Count == 0 || collision.gameObject.tag != "Item")
+            return;
+        foreach (Transform item in new List<Transform>(dropItems)) {
+            if (collision.transform == item.transform) {
+                dropItems.Remove (collision.transform);
+                Destroy (collision.transform.gameObject);
+                return;
+            }
         }
     }
 }
