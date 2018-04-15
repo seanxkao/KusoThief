210a47809d35261ff7e26e621a84b1ae7173903c
JoJorge <jorge13jorge13@gmail.com>
2018-04-15 14:41:46 +0800

    more end condition

---
+++ Assets/Scripts/FSM/GetItemID.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourMachine;
+
+[NodeInfo(category = "Action/Thief/")]
+public class GetItemID : ActionNode {
+
+    public GameObjectVar item;
+    public IntVar ID;
+
+    public override void Start () {
+        base.Start (); 
+        ID.Value = item.Value.GetComponent<Item> ().ID;
+    }
+
+    public override Status Update () {
+        return Status.Success;
+    }
+}
